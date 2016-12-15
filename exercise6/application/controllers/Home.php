d80ae9b1cbe6c8340fd38d7d7fb8e15b4c37a2f6
Amiel <amcuasay@student.apc.edu.ph>
2016-12-15 19:08:55 +0800

    exercise 6 updated

---
+++ exercise6/application/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_CONTROLLER{
+	public function index()
+	{
+		$data['title'] = 'WELCOME TO MYPAGE';
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/homepage');
+		$this->load->view('templates/footer');
+	}
+}
--- exercise6/application/views/templates/footer.php
+++ exercise6/application/views/templates/footer.php
@@ -1,14 +1,7 @@
 
-
-<div class="row">
-    <div class="span12">
-        <div class="well">
-            <center><h3>Copyright &copy; Amiel Cuasay</h3></center>
-        </div>
-    </div>
-</div>
-
-
-</div>
-</body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
