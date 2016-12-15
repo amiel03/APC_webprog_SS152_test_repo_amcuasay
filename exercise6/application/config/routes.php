8e2f12a9768454456d0b5faf3a3eaecc303bfc32
Amiel <amcuasay@student.apc.edu.ph>
2016-12-15 18:41:12 +0800

    exercise 6 updated

--- exercise6/application/config/routes.php
+++ exercise6/application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'pages/homepage';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['news'] = 'news';
@@ -59,4 +59,3 @@ $route['news/edit/(:any)'] = 'news/edit/$1';
 
 $route['news/view/(:any)'] = 'news/view/$1';
 $route['news/(:any)'] = 'news/view/$1';
-$route['mypage/homepage'] = 'mypage/homepage';
