a2812a4ffd0f7932c78abcd483a77986671c764a
Amiel <amcuasay@student.apc.edu.ph>
2016-12-16 11:12:59 +0800

    exercise 6 updated

--- exercise6/application/views/pages/homepage.php
+++ exercise6/application/views/pages/homepage.php
@@ -37,7 +37,7 @@
     <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
     <nav id="sidebar-wrapper">
         <ul class="sidebar-nav">
-            <a id="menu-close" href="#" class="btn btn-light btn-lg hvr-grow pull-right toggle"><i class="fa fa-times"></i></a>
+            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle hvr-grow"><i class="fa fa-times"></i></a>
             <li class="sidebar-brand">
                 <a href="#top" onclick=$("#menu-close").click();>Amiel's Page</a>
             </li>
