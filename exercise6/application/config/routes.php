fb9c8ec308068f7114181115c99af33380705dec
Amiel <amcuasay@student.apc.edu.ph>
2016-12-29 21:07:52 +0800

    exercise 6 updated

--- exercise6/application/config/routes.php
+++ exercise6/application/config/routes.php
@@ -52,10 +52,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'user';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['users'] = 'users';
-$route['user/create'] = 'users/create';
+$route['user'] = 'user';
+$route['user/create'] = 'user/create';
 
-$route['user/edit'] = 'users/edit';
+$route['user/edit/(:any)'] = 'user/edit/$1';
 
-$route['user/view'] = 'users/view';
-$route['user'] = 'users/view';
+$route['user/view/(:any)'] = 'user/view/$1';
+$route['user/(:any)'] = 'user/view/$1';
