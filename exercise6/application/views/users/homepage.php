0fd0f4686a17120b58ed5537daffbad9c89f5f64
Amiel <amcuasay@student.apc.edu.ph>
2016-12-29 18:35:51 +0800

    exercise 6 updated

--- exercise6/application/views/users/homepage.php
+++ exercise6/application/views/users/homepage.php
@@ -1,304 +1,133 @@
-<!DOCTYPE html>
-<html lang="en">
+<h2><?php echo $firstname; ?></h2>
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title> Welcome to MyPage</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/stylish-portfolio.css" rel="stylesheet">
-    <link href="css/hover.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <!-- Navigation -->
-    <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
-    <nav id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle hvr-grow"><i class="fa fa-times"></i></a>
-            <li class="sidebar-brand">
-                <a href="#top" onclick=$("#menu-close").click();>Amiel's Page</a>
-            </li>
-            <li class="sidebar-home">
-                <a href="#top" onclick=$("#menu-close").click();>Home</a>
-            </li>
-            <li class="sidebar-about">
-                <a href="#about" onclick=$("#menu-close").click();>About</a>
-            </li>
-           
-            <li class="sidebar-contact">
-                <a href="#list-unstyled" onclick=$("#menu-close").click();>Contact</a>
-            </li>
-        </ul>
-    </nav>
-
-    <!-- Header -->
-    <header id="top" class="header">
-        <div class="text-vertical-center">
-            <h1 style="font-family:HandTypist">Hi I'm Amiel Cuasay</h1>
-            <h3 style="font-size:18px">You can call me Amiel, Kristian or Cuasay ...</h3>
-            <br>
-            <a href="#about" class="btn btn-dark btn-lg hvr-grow">But wait ... there's more!</a>
-        </div>
-    </header>
-
-    <!-- About -->
-    <section id="about" class="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center" style="font-style:italic;font-family:HandTypist;">
-                    <h2>" Hope is being able to see that there </h2>
-                    <h3> is light despite all of the darkness "</h3>
-                    <h3 style="margin-left:500px"> - Desmond Tutu </h3>
-                </div>
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /.container -->
-    </section>
-
-    <!-- Services -->
-    <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
-    <section id="services" class="services bg-primary">
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-lg-10 col-lg-offset-1">
-                    
-
-	<table border='1' cellpadding='4'>
-	    <tr>
-	        <td><strong>Title</strong></td>
-	        <td><strong>Content</strong></td>
-	        <td><strong>Action</strong></td>
-	    </tr>
-    <?php foreach ($pages as $item): ?>
+<center>
+    <table border='5' cellpadding='4'>
         <tr>
-            <td><?php echo $item['title']; ?></td>
-            <td><?php echo $item['text']; ?></td>
-            <td>
-                <a href="<?php echo site_url('users/'.$item['slug']); ?>">View</a> |
-                <a href="<?php echo site_url('users/edit/'.$item['id']); ?>">Edit</a> |
-                <a href="<?php echo site_url('users/delete/'.$item['id']); ?>" onClick="return confirm('Are you sure you want to delete?')">Delete</a>
-            </td>
+            <td><strong>First Name</strong></td>
+            <td><strong>Last Name</strong></td>
+            <td><strong>Middle Name</strong></td>
+            <td><strong>Nickname</strong></td>
+            <td><strong>Email Address</strong></td>
+            <td><strong>Home Address</strong></td>
+            <td><strong>Gender</strong></td>
+            <td><strong>Comment</strong></td>
+            <td><strong>Action</strong></td>
         </tr>
-    <?php endforeach; ?>
-</table>
-              
-        <!-- /.container -->
-    </section>
+        <?php foreach ($user as $user_item): ?>
+            <tr>
+                <td><?php echo $user_item['firstname']; ?></td>
+                <td><?php echo $user_item['lastname']; ?></td>
+                <td><?php echo $user_item['midname']; ?></td>
+                <td><?php echo $user_item['nickname']; ?></td>
+                <td><?php echo $user_item['email']; ?></td>
+                <td><?php echo $user_item['homeadd']; ?></td>
+                <td><?php echo $user_item['gender']; ?></td>
+                <td><?php echo $user_item['comment']; ?></td>
+                <td>
+                    <a href="<?php echo site_url('user/'.$user_item['slug']); ?>">View</a> |
+                    <a href="<?php echo site_url('user/edit/'.$user_item['id']); ?>">Edit</a> |
+                    <a href="<?php echo site_url('user/delete/'.$user_item['id']); ?>" onClick="return confirm('Are you sure you want to delete?')">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 
-    <!-- Callout -->
-    <!--<aside class="callout">
-        <div class="text-vertical-center">
-            <h1>Vertically Centered Text</h1>
-        </div>
-    </aside>
 
-    <!-- Portfolio -->
-    <!--<section id="portfolio" class="portfolio">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-10 col-lg-offset-1 text-center">
-                    <h2>Our Work</h2>
-                    <hr class="small">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="portfolio-item">
-                                <a href="#">
-                                    <img class="img-portfolio img-responsive" src="img/portfolio-1.jpg">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="portfolio-item">
-                                <a href="#">
-                                    <img class="img-portfolio img-responsive" src="img/portfolio-2.jpg">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="portfolio-item">
-                                <a href="#">
-                                    <img class="img-portfolio img-responsive" src="img/portfolio-3.jpg">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="portfolio-item">
-                                <a href="#">
-                                    <img class="img-portfolio img-responsive" src="img/portfolio-4.jpg">
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <a href="#" class="btn btn-dark">View More Items</a>
-                </div>
-                
-            </div>
-            
-        </div>
-        
-    </section>-->
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title></title>
 
-    <!-- Call to Action -->
-    <!--<aside class="call-to-action bg-primary">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h3>The buttons below are impossible to resist.</h3>
-                    <a href="#" class="btn btn-lg btn-light">Click Me!</a>
-                    <a href="#" class="btn btn-lg btn-dark">Look at Me!</a>
-                </div>
-            </div>
-        </div>
-    </aside>-->
-
-    <!-- Map -->
-    <!--<section id="contact" class="map">
-        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
-        <br />
-        <small>
-            <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
-        </small>
-        </iframe>
-    </section>-->
-
-    <!-- Footer -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-10 col-lg-offset-1 text-center">
-                    <h4><strong>Amiel Cuasay</strong>
-                    </h4>
-                    
-                    <ul class="list-unstyled">
-                        <li><i class="fa fa-phone fa-fw"></i> (123) 456-7890</li>
-                        <li><i class="fa fa-envelope-o fa-fw"></i> <a href="mailto:name@example.com">amcuasay04@gmail.com</a>
-                        </li>
-                    </ul>
-                    <br>
-                    <ul class="list-inline">
-                        <li>
-                            <a href="#"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-youtube fa-fw fa-3x"></i></a>
-                        </li>
-                    </ul>
-                    <hr class="small">
-                    <p class="text-muted">Copyright &copy; Amiel Cuasay 2016</p>
-                </div>
-            </div>
-        </div>
-        <a id="to-top" href="#top" class="btn btn-dark btn-lg"><i class="fa fa-chevron-up fa-fw fa-1x"></i></a>
-    </footer>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script>
-    // Closes the sidebar menu
-    $("#menu-close").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
-    // Opens the sidebar menu
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
-    // Scrolls to the selected menu item on the page
-    $(function() {
-        $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
+        <style>
+            body {margin:0;}
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+                position: fixed;
+                top: 0;
+                width: 100%;
             }
-        });
-    });
-    //#to-top button appears after scrolling
-    var fixed = false;
-    $(document).scroll(function() {
-        if ($(this).scrollTop() > 250) {
-            if (!fixed) {
-                fixed = true;
-                // $('#to-top').css({position:'fixed', display:'block'});
-                $('#to-top').show("slow", function() {
-                    $('#to-top').css({
-                        position: 'fixed',
-                        display: 'block'
-                    });
-                });
+            li {
+                float: left;
             }
-        } else {
-            if (fixed) {
-                fixed = false;
-                $('#to-top').hide("slow", function() {
-                    $('#to-top').css({
-                        display: 'none'
-                    });
-                });
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 20px;
+                text-decoration: none;
             }
-        }
-    });
-    // Disable Google Maps scrolling
-    // See http://stackoverflow.com/a/25904582/1607849
-    // Disable scroll zooming and bind back the click event
-    var onMapMouseleaveHandler = function(event) {
-        var that = $(this);
-        that.on('click', onMapClickHandler);
-        that.off('mouseleave', onMapMouseleaveHandler);
-        that.find('iframe').css("pointer-events", "none");
-    }
-    var onMapClickHandler = function(event) {
-            var that = $(this);
-            // Disable the click handler until the user leaves the map area
-            that.off('click', onMapClickHandler);
-            // Enable scrolling zoom
-            that.find('iframe').css("pointer-events", "auto");
-            // Handle the mouse leave event
-            that.on('mouseleave', onMapMouseleaveHandler);
-        }
-        // Enable map zooming with mouse scroll when the user clicks the map
-    $('.map').on('click', onMapClickHandler);
-    </script>
+            li a:hover:not(.active) {
+                background-color: #111;
+            }
+            .active {
+                background-color: #4CAF50;
+            }
+            .container {
+                position: relative;
+            }
+            .center {
+                position: absolute;
+                left: 0;
+                top: 10%;
+                width: 100%;
+                text-align: center;
+                font-size: 18px;
+            }
+            img {
+                width: 100%;
+                opacity: 0.7;
+            }
+            h1 {
+                text-align: center;
+                text-transform: capitalize;
+                color: #DC143C;
 
-</body>
+            }
+            h3 {
+                text-align: center;
+                text-transform: capitalize;
+                color: #000000;
+                padding-bottom: 0%
 
-</html>
+            }
+            h2 {
+                text-align: center;
+                text-transform: capitalize;
+                color: #000000;
+                padding: 100px 0px
+
+            }
+            .left {
+                position: absolute;
+                right: 300px;
+                width: 400px;
+                border: 3px solid #73AD21;
+                padding: 100px;
+            }
+            p {
+                text-indent:50px;
+                text-align: center;
+                letter-spacing: 2px
+                direction:rt1;
+            }
+            .center {
+                height: 200px;
+                position: relative;
+                border: 3px solid green;
+            }
+            .center p {
+                margin: 0;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                -ms-transform: translate(-50%, -50%);
+                transform: translate(-50%, -50%);
+            }
+
+            a
+            {
+                text-decoration: none;
