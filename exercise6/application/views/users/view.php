d2a8760e08b1f82e7e3c7b0c165b04183caad7e8
Amiel <amcuasay@student.apc.edu.ph>
2016-12-29 18:32:43 +0800

    exercise 6 updated

--- exercise6/application/views/users/view.php
+++ exercise6/application/views/users/view.php
@@ -1,3 +1,10 @@
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css"/>
 <?php
-echo '<h2>'.$news_item['title'].'</h2>';
-echo $news_item['text'];
+echo '<h3>'.$user_item['firstname'].'</h3>';
+echo '<h3>'.$user_item['lastname'].'</h3>';
+echo '<h3>'.$user_item['midname'].'</h3>';
+echo '<h3>'.$user_item['nickname'].'</h3>';
+echo '<h3>'.$user_item['email'].'</h3>';
+echo '<h3>'.$user_item['homeadd'].'</h3>';
+echo '<h3>'.$user_item['gender'].'</h3>';
+echo '<h3>'.$user_item['comment'].'</h3>';
