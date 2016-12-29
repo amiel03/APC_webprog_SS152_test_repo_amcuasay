6565d01cc6c9816ae8e43894a570574f48af498e
Amiel <amcuasay@student.apc.edu.ph>
2016-12-29 17:44:13 +0800

    exercise 7 updated

--- exercise7/config/db.php
+++ exercise7/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=qwerty',
+    'dsn' => 'mysql:host=localhost;dbname=yiidb',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
